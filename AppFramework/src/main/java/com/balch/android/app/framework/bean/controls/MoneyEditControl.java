58369925894f0343a260e6b2e888045c53651f7c
Balch <balch61@sbcglobal.net>
2014-09-07 19:55:26 -0700

    Set correct InputType on the Money Control

--- AppFramework/src/main/java/com/balch/android/app/framework/bean/controls/MoneyEditControl.java
+++ AppFramework/src/main/java/com/balch/android/app/framework/bean/controls/MoneyEditControl.java
@@ -23,17 +23,13 @@
 package com.balch.android.app.framework.bean.controls;
 
 import android.content.Context;
-import android.text.InputFilter;
-import android.text.Spanned;
+import android.text.InputType;
 import android.util.AttributeSet;
 
+import com.balch.android.app.framework.R;
 import com.balch.android.app.framework.bean.BeanColumnDescriptor;
 import com.balch.android.app.framework.bean.BeanViewHint;
 import com.balch.android.app.framework.types.Money;
-
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MoneyEditControl extends StringEditControl {
     protected boolean hideCents = false;
@@ -48,6 +44,13 @@ public class MoneyEditControl extends StringEditControl {
 
     public MoneyEditControl(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        this.value.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        this.value.setHint(R.string.bean_control_money_hint);
     }
 
     @Override
@@ -67,34 +70,6 @@ public class MoneyEditControl extends StringEditControl {
             value = ((Money) obj).getCurrencyNoGroupSep(hideCents?0:2);
         }
         return value;
-    }
-
-    @Override
-    protected List<InputFilter> getInputFilters() {
-        List<InputFilter> filters = super.getInputFilters();
-        filters.add(new CurrencyFormatInputFilter());
-        return filters;
-    }
-
-    // adapted from http://stackoverflow.com/questions/7627148/edittext-with-currency-format
-    public static class CurrencyFormatInputFilter implements InputFilter {
-
-        protected static Pattern mPattern = Pattern.compile("(0|[1-9]+[0-9]*)?(\\.[0-9]{0,2})?");
-
-        @Override
-        public CharSequence filter(CharSequence source, int start, int end,
-                                   Spanned dest, int dstart, int dend) {
-
-            String result = dest.subSequence(0, dstart) +
-                            source.toString() +
-                            dest.subSequence(dend, dest.length());
-
-            Matcher matcher = mPattern.matcher(result);
-
-            return (!matcher.matches()) ?
-                    dest.subSequence(dstart, dend) :
-                    null;
-        }
     }
 
     @Override

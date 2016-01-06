0b39920171c84e282d1dfa5e6ab079cc201f76c2
Balch <balch61@sbcglobal.net>
2016-01-06 15:39:51 -0800

    Broadcast Receiver log message

--- MockTradeApp/src/main/java/com/balch/mocktrade/portfolio/PortfolioUpdateBroadcaster.java
+++ MockTradeApp/src/main/java/com/balch/mocktrade/portfolio/PortfolioUpdateBroadcaster.java
@@ -3,14 +3,15 @@ package com.balch.mocktrade.portfolio;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 public class PortfolioUpdateBroadcaster  {
     private static final String TAG = PortfolioUpdateBroadcaster.class.getSimpleName();
 
     public static final String ACTION = PortfolioUpdateBroadcaster.class.getName();
 
-
     static public void broadcast(Context context) {
+        Log.d(TAG, "broadcast sent:" + ACTION);
         LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ACTION));
     }
 
